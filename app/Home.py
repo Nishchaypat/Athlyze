d312d45b991e697d7e9462d9bbc3992c88fcef5b
Nishchay <nishchay12120@gmail.com>
2025-06-05 20:28:08 -0400

    Added new style

--- app/Home.py
+++ app/Home.py
@@ -8,7 +8,6 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# Try to load custom CSS if it exists
 try:
     local_css("styles.css")
 except FileNotFoundError:
